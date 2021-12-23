2203bc2836803daabf49e5f18c3082d53159b33c
Mario L <m9lopez@ucsd.edu>
2021-12-23 00:39:03 -0800

    on part 15

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import Header from "./components/Header"
+import FeedbackItem from "./components/FeedbackItem"
 function App() {
+
+    
     return(
+        <>
+        <Header  />
         <div className='container'>
-            <h1> Hello from the app componet</h1>
+            {/* <h1> Hello from the app componet</h1> */}
+            <FeedbackItem />
         </div>
+        
+        
+        </>
     )
 }
 
